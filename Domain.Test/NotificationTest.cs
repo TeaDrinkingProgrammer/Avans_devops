77c8f96ae5f73cf9d14a028f8cbc36d4c591cc88
FSGabrsek <f.gabrsek@gmail.com>
2023-04-01 10:23:03 +0200

    added notification tests

---
+++ Domain.Test/NotificationTest.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Notifier;
+using Domain.Notifier.Events;
+using NSubstitute;
+
+namespace Domain.Test;
+
+public class NotificationTest
+{
+    [Fact]
+    public void NotificationServiceShouldSendEmailNotifications()
+    {
+        var notificationWriter = Substitute.For<IWriter>();
+        var notificationService = new NotificationService(new EmailService(notificationWriter), new SlackService(notificationWriter));
+        var teamMember = new TeamMember("John Doe", "johndoe@mail.com", "johndoe");
+        
+        notificationService.OnNext(new Notification(teamMember, "Hello World!"));
+        
+        notificationWriter.Received().WriteLine("To: John Doe <johndoe@mail.com>: Hello World!");
+    }
+    
+    [Fact]
+    public void NotificationServiceShouldSendSlackNotifications()
+    {
+        var notificationWriter = Substitute.For<IWriter>();
+        var notificationService = new NotificationService(new EmailService(notificationWriter), new SlackService(notificationWriter));
+        var teamMember = new TeamMember("John Doe", "johndoe@mail.com", "johndoe");
+        
+        notificationService.OnNext(new Notification(teamMember, "Hello World!"));
+        
+        notificationWriter.Received().WriteLine("@<johndoe>: Hello World!");
+    }
+}
